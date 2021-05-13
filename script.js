80b413a150460784ad7a2c194a141ad441c5113d
Rewa Patil <>
2021-05-13 02:01:21 +0000

    add parseInts

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 const number = 12
 let guess = 0
-guess = parseInt(prompt('enter your guess'))
-while (guess != 12) {
-  if (guess < 12) {
-    guess = prompt('Your guess is too small, please try again')
+guess = parseInt(prompt('enter your guess'), 10)
+while (guess !== number) {
+  if (guess < number) {
+    guess = parseInt(prompt('Your guess is too small, please try again'), 10)
   } else {
-    guess = prompt('Your guess is too big, please try again')
+    guess = parseInt(prompt('Your guess is too big, please try again'), 10)
   }
 }
 alert('You guessed correctly! Game over')
